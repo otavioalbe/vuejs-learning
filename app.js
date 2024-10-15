f19c713192e2205c3b1967921131dfd3c6419d32
Otávio De Carli Albuquerque <otavioalbe@gmail.com>
2024-10-14 22:20:44 -0300

    variavel impressa no HTML

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
     // data, functions
     // template: '<h2>Isso é um template</h2>'
+    data() {
+        return {
+            title: 'The Final Empire'
+        }
+    }
 });
 
 app.mount('#app');
