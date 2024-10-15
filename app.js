bef648eca87a477aa6192eafbebf7409f419bda9
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-15 14:06:16 -0300

    methods do componente

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
             autor: 'Brandon Sanderson',
             idade: 45
         }
+    },
+    methods: {
+        trocarTitulo(titulo) {
+            this.titulo = titulo;
+        }
     }
 });
 
