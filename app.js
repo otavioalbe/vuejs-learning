ed30c83252d5502410c9e784a4a81abbd9e42ff9
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-15 16:51:11 -0300

    mouse events (mouseover, mouseleave, dblclick e mousemove

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ const app = Vue.createApp({
             mostrarLivros: true,
             titulo: 'The Final Empire',
             autor: 'Brandon Sanderson',
-            idade: 45
+            idade: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleMostrarLivros() {
-            this.mostrarLivros = !this.mostrarLivros
+            this.mostrarLivros = !this.mostrarLivros;
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 });
