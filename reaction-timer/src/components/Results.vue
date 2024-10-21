d6fcf1ffce83dd17e67068d48e4a16b0875c4d09
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-21 01:01:15 -0300

    projeto teste de reação finalizado

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
-    <p>Tempo de reação: {{ score }} </p>
+    <p>Tempo de reação: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = 'Dedos de ninja'
+        } else if(this.score < 400 ){
+            this.rank = 'Reflexos apurados'
+        } else{
+            this.rank = 'Lerdo...'
+        }
+    }
 }
 </script>
 
 <style>
-
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
