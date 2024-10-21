03bb4a04d9b2daa8e7f1497e64798d414695f20b
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-21 09:58:35 -0300

    criação projeto web form

---
+++ web-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SingUpFormVue/>
+</template>
+
+<script>
+import SingUpFormVue from './components/SingUpForm.vue'
+
+export default {
+  name: 'App',
+  components: { SingUpFormVue }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin:0;
+  background: #eee;
+}
+
+</style>
