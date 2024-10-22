449e7dc4208cdede8ff76b97cb5feb4348268fb5
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 16:16:06 -0300

    computed values

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ personOne.name }} {{ personOne.age }}</p>
-    <button @click="updatePersonAge">update person1</button>
-    <h2>Reactive</h2>
-    <p>{{ personTwo.name }} {{ personTwo.age }} {{ nameTwo }}</p>
-    <button @click="updatePersonAgeTwo">update person2</button>
+    <input type="text" v-model="search">
+    <p>{{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+        {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 
 export default {
   name: 'HomeView',
   setup(){
-    const personOne = ref({ name: 'mario', age: 30 })
-    const personTwo = reactive({ name: 'luigi', age: 35 })
+    const names = ref(['mario','vans','yoshi','fabris','claat','bomba',])
+    const search = ref('')
 
-    const nameOne = ref('mario')
-    const nameTwo = reactive('luigi')
+    const matchingNames = computed(() => {
+      return names.value.filter((name) =>
+        name.includes(search.value))
+    })
 
-    const updatePersonAge = () => {
-      personOne.value.age = 40
-    }
-    const updatePersonAgeTwo = () => {
-      personTwo.age = 50
-      // nameTwo = 'carlos' -> não funciona
-    }
-
-    return { personOne, personTwo, updatePersonAge, updatePersonAgeTwo, nameTwo }
+    return { names, search, matchingNames }
   }
 }
 </script>
