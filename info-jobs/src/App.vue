1affe190a98c5ee802a74e6c1c190e7c5ebf521a
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 01:29:02 -0300

    folder structures

--- info-jobs/src/App.vue
+++ info-jobs/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
   <router-view/>
 </template>
@@ -22,9 +23,14 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  margin: 0px 5px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  background: crimson;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  text-decoration: none;
 }
 </style>
