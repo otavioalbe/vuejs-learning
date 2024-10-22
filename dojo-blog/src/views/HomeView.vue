ccde728ab8429a4f7ea7b368d469fe0109f49acd
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 16:24:05 -0300

    watch / watchEffect

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -6,11 +6,12 @@
     <div v-for="name in matchingNames" :key="name">
         {{ name }}
     </div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -18,12 +19,25 @@ export default {
     const names = ref(['mario','vans','yoshi','fabris','claat','bomba',])
     const search = ref('')
 
+    const stopWatch = watch(search, () => {
+      console.log('watch function run')
+    })
+
+    const stopWatchEffect = watchEffect(() => {
+      console.log('watchEffect function run', search.value)
+    })
+
     const matchingNames = computed(() => {
       return names.value.filter((name) =>
         name.includes(search.value))
     })
 
-    return { names, search, matchingNames }
+    const handleClick = () => {
+      stopWatch()
+      stopWatchEffect()
+    }
+
+    return { names, search, matchingNames, handleClick }
   }
 }
 </script>
