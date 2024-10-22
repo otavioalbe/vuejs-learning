bb1ba60af03eb16ef46dd86f0fb469487f2c5534
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 14:35:44 -0300

    composition API basics

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    home
+    {{ name }} {{ age }}
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  setup(){
+    console.log('setup')
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    let name = 'mario'
+    let age = 30
+    return { name, age, handleClick }
+  }
+}
+</script>
