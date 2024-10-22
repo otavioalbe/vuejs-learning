e22eef1441d88913e2a27bad2dfe19c9e21fb295
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 14:43:39 -0300

    template refs (composition)

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="home">
     home
-    {{ name }} {{ age }}
+    <p ref="p">{{ name }} {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   setup(){
     console.log('setup')
 
+    const p = ref(null)
+
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello world!'
     }
 
     let name = 'mario'
     let age = 30
-    return { name, age, handleClick }
+    return { name, age, handleClick, p}
   }
 }
 </script>
