26e05ab4d0a5fba9ced5ad316284fe6c58498eed
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 01:56:24 -0300

    redirects and 404 (catchAll)

--- info-jobs/src/router/index.js
+++ info-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import JobsView from '../views/jobs/JobsView.vue'
 import JobDetails from '@/views/jobs/JobDetails.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {
@@ -25,6 +26,17 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
