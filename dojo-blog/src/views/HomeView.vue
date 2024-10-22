9547d770de7e4d0da026461f4fdedf52586eaafa
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-22 15:59:05 -0300

    refs x reactive

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="home">
-    home
-    <p>{{ name }} {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ personOne.name }} {{ personOne.age }}</p>
+    <button @click="updatePersonAge">update person1</button>
+    <h2>Reactive</h2>
+    <p>{{ personTwo.name }} {{ personTwo.age }} {{ nameTwo }}</p>
+    <button @click="updatePersonAgeTwo">update person2</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'HomeView',
   setup(){
+    const personOne = ref({ name: 'mario', age: 30 })
+    const personTwo = reactive({ name: 'luigi', age: 35 })
 
-    let name = ref('mario')
-    let age = ref(30)
+    const nameOne = ref('mario')
+    const nameTwo = reactive('luigi')
 
-    const handleClick = () => {
-      name.value = 'luigi'
-      age.value = 35
+    const updatePersonAge = () => {
+      personOne.value.age = 40
+    }
+    const updatePersonAgeTwo = () => {
+      personTwo.age = 50
+      // nameTwo = 'carlos' -> não funciona
     }
 
-    
-    return { name, age, handleClick}
+    return { personOne, personTwo, updatePersonAge, updatePersonAgeTwo, nameTwo }
   }
 }
 </script>
