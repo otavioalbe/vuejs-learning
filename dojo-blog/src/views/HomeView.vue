985ca2add8f127401e385b723e4afbe95497b072
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-23 03:17:14 -0300

    reusable composable functions

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -11,28 +11,14 @@
 
 <script>
 import PostList from '../components/PostList.vue'
-import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'HomeView',
   components: { PostList },
   setup(){
-    const posts = ref([])
-    const error = ref(null)
+    const {posts, error, load} = getPosts()
 
-    const load = async() => {
-      try{
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok){
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
     load()
 
     return { posts, error }
