10b76399d7d1991b9c7ce672c320b70157b9c2d5
Otávio Albuquerque <otavioalbe@gmail.com>
2024-10-23 02:37:07 -0300

    fetchin data in setup

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts"/>
-    <button @click="showPosts = !showPosts">toggle post</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -15,14 +17,25 @@ export default {
   name: 'HomeView',
   components: { PostList },
   setup(){
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet ligula non nisl sagittis vehicula ac vitae sapien. Pellentesque purus ante, interdum sed tincidunt sit amet, convallis non eros. Vestibulum lacinia erat eu est euismod dapibus. In eget ligula sit amet est consequat aliquet. Nunc dapibus pulvinar turpis, eu laoreet tellus laoreet non. Aliquam a elit nec ipsum euismod suscipit. Sed finibus accumsan tortor vel maximus. Mauris aliquet et eros vitae faucibus. Mauris scelerisque cursus quam eu elementum. Sed venenatis interdum faucibus. Nullam sodales nunc in libero pellentesque, sed condimentum mauris mollis. Aliquam tincidunt, metus ut elementum scelerisque, est magna varius mauris, ac feugiat tellus odio ut tellus.', id: 1 },
-      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 },
-      ])
+    const posts = ref([])
+    const error = ref(null)
 
-    const showPosts = ref(true)
+    const load = async() => {
+      try{
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok){
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+    load()
 
-    return { posts, showPosts }
+    return { posts, error }
   }
 }
 </script>
